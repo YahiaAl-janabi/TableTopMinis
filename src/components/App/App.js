49270fb72b8b937ce124f9494dc580fdb85b0167
Yahia Al-janabi <yahiaaljanabi@gmail.com>
2021-03-13 06:06:53 +0000

    up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
-  return <div>1</div>;
+  return (
+    <div className="container">
+      <div className="header">1</div>
+      <div className="main">2</div>
+      <div className="footer">3</div>
+    </div>
+  );
 };
 
 export default App;
