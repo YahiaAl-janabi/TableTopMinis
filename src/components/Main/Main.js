be0d9bb37333cb73c779296ad12bf9b44f47a403
Yahia Al-janabi <yahiaaljanabi@gmail.com>
2021-03-20 16:09:05 +0000

    up

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Main.css";
 import Slider from "../Main/ImageSlide/ImageSlide";
-
+import MainBoutPic from "../Main/MainAboutPic/MainAboutPic";
 const Main = () => {
   return (
     <div className="maincontainer">
       <div className="headimgviewer">
         <Slider />
       </div>
+      <MainBoutPic />
     </div>
   );
 };
---
+++ src/components/Main/MainAboutPic/MainAboutPic.js
@@ -0,0 +1,12 @@
+import React from "react";
+import riyathe from "./riyathe.jpg";
+import "./MainAboutPic.css";
+const MainAboutPic = () => {
+  return (
+    <div className="mainaboutpic">
+      <img className="riyathe" src={riyathe} alt="riyathe" />
+    </div>
+  );
+};
+
+export default MainAboutPic;
