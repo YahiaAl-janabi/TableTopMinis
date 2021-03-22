6448e378b1be0f98e63934aece06b6a253846ec8
Yahia Al-janabi <yahiaaljanabi@gmail.com>
2021-03-22 07:51:37 +0000

    up

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Main.css";
 import Slider from "../Main/ImageSlide/ImageSlide";
 import MainBoutPic from "../Main/MainAboutPic/MainAboutPic";
+import About1 from "./About1/About1";
 const Main = () => {
   return (
     <div className="maincontainer">
       <div className="headimgviewer">
         <Slider />
       </div>
+      <About1 />
       <MainBoutPic />
     </div>
   );
