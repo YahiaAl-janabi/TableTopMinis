8ee011ce8762a3089629349023ecb1d06d8c7ff0
Yahia Al-janabi <yahiaaljanabi@gmail.com>
2021-03-26 19:03:05 +0000

    up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,14 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 import "./App.css";
+import Main2 from "../Main2/Main2";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Main />
+      <Main2 />
       <Footer />
     </div>
   );
---
+++ src/components/Main2/Main2.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Main2.css";
+
+const Main2 = () => {
+  return (
+    <div className="main2container">
+      <div></div>
+    </div>
+  );
+};
+
+export default Main2;
