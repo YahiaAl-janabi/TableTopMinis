8629d439d5c684a3cad8d4d6b9c202e4fa4ddfed
Yahia Al-janabi <yahiaaljanabi@gmail.com>
2021-03-29 15:59:27 +0100

    up

---
+++ src/components/Main2/ImageCardComponents/ImageCard1.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ImageCard.css";
+
+const ImageCard1 = () => {
+  return <div className="imagecard1"></div>;
+};
+
+export default ImageCard1;
--- src/components/Main2/Main2.js
+++ src/components/Main2/Main2.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Main2.css";
+import ImageCardComponents from "./ImageCardComponents/ImageCard1";
 
 const Main2 = () => {
   return (
     <div className="main2container">
-      <div></div>
+      <ImageCardComponents />
     </div>
   );
 };
